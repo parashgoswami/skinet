406738becd377a1ff4f37eb20041ed8aa111d249
Parash Goswami <parashgoswami@gmail.com>
2020-10-13 12:51:06 +0530

    End of section 9

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -1,18 +1,21 @@
+import { ShopModule } from './shop/shop.module';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
-  declarations: [AppComponent, NavBarComponent],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    CoreModule,
+    ShopModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
